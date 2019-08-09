87721efbc4932107642ba969f6b81389effc4ade
Nham Le <lehoainham@gmail.com>
2019-08-09 11:36:57 +0700

    Cleanup folder before dump and better log messages

--- backup.sh
+++ backup.sh
@@ -29,6 +29,10 @@ function get_all_db() {
   echo "$_dbs"
 }
 
+function _info() {
+  echo "$(date) INFO: $1"
+}
+
 # .pgpass format
 # address:port:database:user:password
 # address, port and database could be wildcard
@@ -37,28 +41,29 @@ while IFS= read -r line || [[ -n $line ]]; do
   IFS=: read _hostname _port _database _username _ <<< $line
 
   _backup_dir="${_DUMP_DIR}/${_hostname}"
-  mkdir -p $_backup_dir
+  rm -rf "$_backup_dir"
+  mkdir -p "$_backup_dir"
 
   while read -r _db; do
-    printf "Dump database $_db of server ${_hostname} to folder ${_backup_dir}"
+    _info "Dump database $_db of server ${_hostname} to folder ${_backup_dir}"
     pg_dump \
       -h $_hostname \
       -p $_port \
       -U $_username \
       $_db \
-      >> ${_backup_dir}/${_db}.sql
+      > ${_backup_dir}/${_db}.sql
   done <<< "$(get_all_db $_hostname $_port $_username)"
 
-  echo "Compress folder $_backup_dir"
+  _info "Compress folder $_backup_dir"
   tar \
     cvfz \
     ${_hostname}.gzip \
     $_backup_dir
 
-  echo "Upload file ${_hostname}.gzip to bucket ${S3_BUCKET}"
+  _info "Upload file ${_hostname}.gzip to bucket ${S3_BUCKET}"
   aws s3 cp ${_hostname}.gzip ${_UPLOAD_DIR}/${_hostname}.gzip
 
-  echo "Cleanup dump file"
+  _info "Cleanup dump file"
   rm -rf $_backup_dir
   rm -f ${_hostname}.gzip
 
